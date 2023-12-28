0832a8f4711d8dc6b3f1ac77a4f404b9cdecd36f
EhzandHerry <ehzand.herry1926@gmail.com>
2023-12-28 18:23:38 +0700

    menambahkan function deleteKontak pada class NetworkKontakRepository

--- app/src/main/java/com/example/activity8/repository/KontakRepository.kt
+++ app/src/main/java/com/example/activity8/repository/KontakRepository.kt
@@ -6,11 +6,26 @@ import java.io.IOException
 
 interface KontakRepository {
     suspend fun getKontak(): List<Kontak>
+    suspend fun deleteKontak(id: Int)
 }
 
 class NetworkKontakRepository(
     private val kontakApiService: KontakService
 ) : KontakRepository{
     override suspend fun getKontak(): List<Kontak> = kontakApiService.getKontak()
+
+    override suspend fun deleteKontak(id: Int){
+        try {
+            val response = kontakApiService.deleteKontak(id)
+            if (!response.isSuccessful){
+                throw IOException("Failed to delete kontak. Http status code: ${response.code()}")
+            }
+            else{
+                response.message()
+            }
+        } catch (e: Exception){
+            throw e
+        }
+    }
 
 }
